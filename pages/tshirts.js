907d1f84a6684a4759f45a604e42ca4c702fdd6f
Happy Kamboj <happykamboj@gmail.com>
2023-02-26 00:26:56 +0530

    adding stop scrolling

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -5,6 +5,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Interest from "./interest";
 import Link from "next/link";
+
 // import Product from "@/models/Product";
 // import mongoose from "mongoose";
 const Tshirts = () => {
@@ -50,9 +51,48 @@ const Tshirts = () => {
     // setTenth(res.study);
     // console.log("tenth", tenth);
   };
+  // toggle content
+  const togglebuttton1 = () => {
+    let elems = document.querySelectorAll(".nda");
+    [].forEach.call(elems, function (el) {
+      el.classList.toggle("hidden");
+    });
+
+    let elems1 = document.querySelectorAll(".lll");
+    [].forEach.call(elems1, function (el) {
+      el.classList.toggle("blur-lg");
+      el.classList.toggle("pointer-events-none");
+      if (el.classList.contains("blur-lg")) {
+        window.onscroll = function () {
+          window.scrollTo(0, 0);
+        };
+      } else {
+        window.onscroll = function () {};
+      }
+    });
+  };
+  const togglebuttton2 = () => {
+    let elems = document.querySelectorAll(".tes");
+    [].forEach.call(elems, function (el) {
+      el.classList.toggle("hidden");
+    });
+
+    let elems1 = document.querySelectorAll(".lll");
+    [].forEach.call(elems1, function (el) {
+      el.classList.toggle("blur-lg");
+      el.classList.toggle("pointer-events-none");
+      if (el.classList.contains("blur-lg")) {
+        window.onscroll = function () {
+          window.scrollTo(0, 0);
+        };
+      } else {
+        window.onscroll = function () {};
+      }
+    });
+  };
   return (
     <>
-      <section className="container m-auto">
+      <section className="lll container m-auto">
         <Interest />
         {(props.army === "Indian Army" || props.army === "Forces Type") && (
           <>
@@ -85,7 +125,10 @@ const Tshirts = () => {
                       props.agedate == 0) ||
                     props.ageyear === "-") && (
                     <div className="flex flex-wrap justify-center items-center my-5">
-                      <div className="xl:w-1/3 lg:w-1/3 md:w-1/2 w-full px-8 my-2 mx-2 py-6 border-l-2 border-gray-200 border-opacity-60 bg-slate-300 rounded-xl shadow-sm hover:bg-stone-300 hover:shadow-lg hover:rounded-md">
+                      <div
+                        onClick={togglebuttton1}
+                        className="xl:w-1/3 lg:w-1/3 md:w-1/2 w-full px-8 my-2 mx-2 py-6 border-l-2 border-gray-200 border-opacity-60 bg-slate-300 rounded-xl shadow-sm hover:bg-stone-300 hover:shadow-lg hover:rounded-md cursor-pointer"
+                      >
                         <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
                           National Defence Academy
                         </h2>
@@ -99,25 +142,11 @@ const Tshirts = () => {
                             <b>Qualification - </b> 10+2
                           </div>
                         </div>
-                        <Link
-                          href={"/"}
-                          className="text-indigo-500 inline-flex items-center"
-                        >
-                          Learn More
-                          <svg
-                            fill="none"
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            className="w-4 h-4 ml-2"
-                            viewBox="0 0 24 24"
-                          >
-                            <path d="M5 12h14M12 5l7 7-7 7"></path>
-                          </svg>
-                        </Link>
                       </div>
-                      <div className="xl:w-1/3 lg:w-1/3 md:w-1/2 w-full px-8 my-2 mx-2 py-6 border-l-2 border-gray-200 border-opacity-60 bg-slate-300 rounded-xl shadow-sm hover:bg-stone-300 hover:shadow-lg hover:rounded-md">
+                      <div
+                        onClick={togglebuttton2}
+                        className="xl:w-1/3 lg:w-1/3 md:w-1/2 w-full px-8 my-2 mx-2 py-6 border-l-2 border-gray-200 border-opacity-60 bg-slate-300 rounded-xl shadow-sm hover:bg-stone-300 hover:shadow-lg hover:rounded-md cursor-pointer"
+                      >
                         <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
                           Technical Entry Scheme
                         </h2>
@@ -131,23 +160,6 @@ const Tshirts = () => {
                             <b>Qualification - </b> 10+2 (70% marks with PCM)
                           </div>
                         </div>
-                        <Link
-                          href={"/"}
-                          className="text-indigo-500 inline-flex items-center"
-                        >
-                          Learn More
-                          <svg
-                            fill="none"
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            className="w-4 h-4 ml-2"
-                            viewBox="0 0 24 24"
-                          >
-                            <path d="M5 12h14M12 5l7 7-7 7"></path>
-                          </svg>
-                        </Link>
                       </div>
                     </div>
                   )}
@@ -160,6 +172,47 @@ const Tshirts = () => {
           </>
         )}
       </section>
+      {/* pop up data */}
+      <div className="flex flex-wrap justify-center items-center my-5">
+        <div className="container nda hidden border border-black mx-auto md:top-1/3 bg-slate-400 fixed z-50 justify-center text-center md:w-2/3 lg:w-1/2 md:h-1/2 w-5/6 h-1/2 top-1/4 overflow-auto">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique
+          hic velit veniam cum minima aliquam at. Consequatur tenetur fugit eos
+          sed dolor corporis, blanditiis minima provident, voluptatum eligendi
+          adipisci atque repellendus veritatis reprehenderit. Impedit, hic neque
+          temporibus architecto nulla voluptatum unde consequatur repellendus!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+          deleniti ex Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Aut odio ipsa fugiat quis tempore possimus, recusandae dolor obcaecati
+          iure! Ipsa dolores pariatur est necessitatibus tenetur! Delectus
+          tenetur hic placeat excepturi? Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Odio dignissimos animi, doloremque distinctio
+          corrupti aut quia nostrum suscipit excepturi impedit recusandae iste
+          unde rerum eius exercitationem, molestias quis libero repellat
+          officiis officia! Iure animi repellat odio sapiente aliquid
+          cupiditate! Dolore quia porro ea aspernatur accusantium iure ratione
+          illum eius expedita!
+          <div onClick={togglebuttton1}> xxxx</div>
+        </div>
+        <div className="container tes hidden border border-black mx-auto md:top-1/3 bg-slate-400 fixed z-50 justify-center text-center md:w-2/3 lg:w-1/2 md:h-1/2 w-5/6 h-1/2 top-1/4 overflow-auto">
+          tes Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Similique hic velit veniam cum minima aliquam at. Consequatur tenetur
+          fugit eos sed dolor corporis, blanditiis minima provident, voluptatum
+          eligendi adipisci atque repellendus veritatis reprehenderit. Impedit,
+          hic neque temporibus architecto nulla voluptatum unde consequatur
+          repellendus! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Numquam deleniti ex Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Aut odio ipsa fugiat quis tempore possimus,
+          recusandae dolor obcaecati iure! Ipsa dolores pariatur est
+          necessitatibus tenetur! Delectus tenetur hic placeat excepturi? Lorem
+          ipsum dolor, sit amet consectetur adipisicing elit. Odio dignissimos
+          animi, doloremque distinctio corrupti aut quia nostrum suscipit
+          excepturi impedit recusandae iste unde rerum eius exercitationem,
+          molestias quis libero repellat officiis officia! Iure animi repellat
+          odio sapiente aliquid cupiditate! Dolore quia porro ea aspernatur
+          accusantium iure ratione illum eius expedita!
+          <div onClick={togglebuttton2}> xxxx</div>
+        </div>
+      </div>
     </>
   );
 };
