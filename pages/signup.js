10c8e6a64b772b95db39b792c262e32d12e58ba8
madneg <126816095+madneg@users.noreply.github.com>
2023-03-03 20:04:32 +0530

    Update signup.js

--- pages/signup.js
+++ pages/signup.js
@@ -30,7 +30,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const data = { name, email, password, confirmpassword };
-    let res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/signup`, {
+    let res = await fetch(`${process.env.VERCEL_URL}/api/signup`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
